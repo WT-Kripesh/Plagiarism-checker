f007a2e2474b635382ea9aa4cdd79f0947689b98
Kripesh Nihure <nkripesh0@gmail.com>
2024-02-23 23:59:13 +0545

    google signin added

--- classroom/src/App.js
+++ classroom/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Home from "./components/home";
 import { BrowserRouter as 
   Router, Route, Routes} from "react-router-dom";
+
 
 function App() {
   return <div className="app">
     <Router>
       <Routes>
-        <Route exact path="/test">
-          Hello
-        </Route>
+        <Route exact path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   </div>;
---
+++ classroom/src/components/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./home.css";
+import classroomgif from "./gif.gif"
+
+function home() {
+  return (
+    <div className="home">
+  <div className="home__container">
+    <img
+      src= {classroomgif} //"https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png"
+      alt="Google Classroom Image"
+      className="home__image"
+    />
+    <button className="home__login">
+      Login with Google
+    </button>
+  </div>
+</div>
+  )
+}
+
+export default home
