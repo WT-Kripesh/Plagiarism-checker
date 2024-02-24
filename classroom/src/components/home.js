6c31aec809fd3b354e47d93a3105909df157cc24
Kripesh Nihure <nkripesh0@gmail.com>
2024-02-24 18:38:52 +0545

    dashboard added in home

--- classroom/src/components/home.js
+++ classroom/src/components/home.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import './styles/home.css';
-import { signInWithGoogle } from './firebase';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import "./styles/home.css";
+import { auth, db, logout } from "./firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
 import classroomlogo from './logo.png';
 
-function Home() {
+function Dashboard() {
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setName(data.name);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/");
+    fetchUserName();
+  }, [user, loading]);
   return (
-    <div className="home">
-      <div className="home__container">
-        <img
+    <div className="dashboard">
+       <div className="dashboard__container">
+       <img
           src={classroomlogo}
           alt="Google Classroom Image"
-          className="home__image"
+          className="image"
         />
-        <button className="home__login" onClick={signInWithGoogle} >
-          Login with Google
-        </button>
-      </div>
-    </div>
+        Logged in as
+         <div>{name}</div>
+         <div>{user?.email}</div>
+         <button className="dashboard__btn" onClick={logout}>
+          Logout
+         </button>
+       </div>
+     </div>
   );
 }
-
-export default Home;
+export default Dashboard;
