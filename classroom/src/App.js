2ac407be564e65c89d7c4dacdd6a6f9e1ee11d05
Kripesh Nihure <nkripesh0@gmail.com>
2024-02-25 14:27:00 +0545

    Unnecessary files removed

--- classroom/src/App.js
+++ classroom/src/App.js
@@ -4,7 +4,6 @@ import Dashboard from "./screens/Dashboard";
 import Register from "./screens/register"
 import Navbar from './components/navbar';
 import Class from './screens/Class';
-import Home from './screens/home'
 import { BrowserRouter as 
   Router, Route, Routes} from "react-router-dom";
 
@@ -12,8 +11,7 @@ function App() {
   return <div className="app">
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/home" element={<Home />} />
+      <Route exact path="/" element={<Login />} />
       <Route exact path="/register" element={<Register />} />
       <Route excat path="/dashboard" element={
         <>
